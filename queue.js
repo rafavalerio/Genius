57d1ecea3dbfbf61ecf7797466521dffd00ad737
Guilherme Ferreira <guilffer@gmail.com>
2011-11-05 17:21:55 -0200

    a simple queue to control the blink events was added

---
+++ queue.js
@@ -0,0 +1,19 @@
+function Queue() {
+	this.arrQ = new Array();
+	this.idQ = 0;
+	
+	this.add = function(func){
+    	this.arrQ.push(func);
+    	if( (this.idQ+1) == this.arrQ.length ) 
+    		this.run();
+    };
+    
+    this.run = function(){
+    	(this.arrQ[this.idQ])();
+    };
+    
+    this.next = function() {
+    	if (++(this.idQ) < this.arrQ.length)
+    		this.run();
+    };
+};
